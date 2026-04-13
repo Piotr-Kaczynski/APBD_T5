ff19adc96ecd02622737535aaa4f31d36a9f0f86
piotrkowskyyy <piotr.kaczynski10@gmail.com>
2026-04-13 21:13:23 +0200

    tax rate class for seperation taxes throygh class, not elseif statements

---
+++ LegacyRenewalApp/TaxRateProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace LegacyRenewalApp
+{
+    public class TaxRateProvider
+    {
+        private const decimal DefaultTaxRate = 0.20m;
+
+        private static readonly Dictionary<string, decimal> RatesByCountry =
+            new Dictionary<string, decimal>
+            {
+                { "Poland",         0.23m },
+                { "Germany",        0.19m },
+                { "Czech Republic", 0.21m },
+                { "Norway",         0.25m },
+            };
+
+        public decimal GetRate(string country)
+        {
+            return RatesByCountry.TryGetValue(country, out decimal rate) ? rate : DefaultTaxRate;
+        }
+    }
+}
